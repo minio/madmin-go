36a0511827f0f528f7c31148bc585eeb761eafcd
Aditya Manthramurthy <aditya.mmy@gmail.com>
2022-08-24 12:02:47 -0700

    Fix bug in config parsing

--- parse-config_test.go
+++ parse-config_test.go
@@ -100,6 +100,85 @@ subnet license=1 api_key= proxy=`,
 				},
 			},
 		},
+		{
+			Name: "multiple targets no env",
+			Config: `logger_webhook enable=off endpoint= auth_token= client_cert= client_key= queue_size=100000
+logger_webhook:1 endpoint=http://localhost:8080/ auth_token= client_cert= client_key= queue_size=100000
+`,
+			Expected: []SubsysConfig{
+				{
+					SubSystem: LoggerWebhookSubSys,
+					Target:    "",
+					KV: []ConfigKV{
+						{
+							Key:   "enable",
+							Value: "off",
+						},
+						{
+							Key:   "endpoint",
+							Value: "",
+						},
+						{
+							Key:   "auth_token",
+							Value: "",
+						},
+						{
+							Key:   "client_cert",
+							Value: "",
+						},
+						{
+							Key:   "client_key",
+							Value: "",
+						},
+						{
+							Key:   "queue_size",
+							Value: "100000",
+						},
+					},
+					kvIndexMap: map[string]int{
+						"enable":      0,
+						"endpoint":    1,
+						"auth_token":  2,
+						"client_cert": 3,
+						"client_key":  4,
+						"queue_size":  5,
+					},
+				},
+				{
+					SubSystem: LoggerWebhookSubSys,
+					Target:    "1",
+					KV: []ConfigKV{
+						{
+							Key:   "endpoint",
+							Value: "http://localhost:8080/",
+						},
+						{
+							Key:   "auth_token",
+							Value: "",
+						},
+						{
+							Key:   "client_cert",
+							Value: "",
+						},
+						{
+							Key:   "client_key",
+							Value: "",
+						},
+						{
+							Key:   "queue_size",
+							Value: "100000",
+						},
+					},
+					kvIndexMap: map[string]int{
+						"endpoint":    0,
+						"auth_token":  1,
+						"client_cert": 2,
+						"client_key":  3,
+						"queue_size":  4,
+					},
+				},
+			},
+		},
 	}
 
 	for i, test := range tests {
@@ -112,7 +191,7 @@ subnet license=1 api_key= proxy=`,
 			continue
 		}
 		if !reflect.DeepEqual(test.Expected, r) {
-			t.Errorf("Test %d (%s) expected %#v got %#v\n", i, test.Name, test.Expected, r)
+			t.Errorf("Test %d (%s) expected:\n%#v\nbut got:\n%#v\n", i, test.Name, test.Expected, r)
 		}
 	}
 }
