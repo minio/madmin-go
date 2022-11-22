30ff231c006ccc03facef57885ecd881c5f5f8f2
Harshavardhana <harsha@minio.io>
2022-11-22 10:47:58 -0800

    Change madmin-go license to AGPLv3
    Also bump v2.0.0 version

--- cgroup/linux.go
+++ cgroup/linux.go
@@ -1,3 +1,22 @@
+//
+// Copyright (c) 2015-2022 MinIO, Inc.
+//
+// This file is part of MinIO Object Storage stack
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+
 //go:build linux
 // +build linux
 
@@ -35,7 +54,7 @@ import (
 )
 
 // DO NOT EDIT following constants are chosen defaults for any kernel
-// after 3.x, please open a GitHub issue https://github.com/minio/madmin-go/issues
+// after 3.x, please open a GitHub issue https://github.com/minio/madmin-go/v2/issues
 // and discuss first if you wish to change this.
 const (
 	// Default string for looking for kernel memory param.
