6993bdfd91871ffdf1345f2bc404c25846b57e20
Poorna Krishnamoorthy <poornas@users.noreply.github.com>
2023-05-05 17:24:26 -0700

    replication: Add MRF backlog API
    Return most recent failed replication entries

---
+++ examples/replicate-mrf.go
@@ -0,0 +1,50 @@
+//go:build ignore
+// +build ignore
+
+// Copyright (c) 2015-2023 MinIO, Inc.
+//
+// This file is part of MinIO Object Storage stack
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/minio/madmin-go/v2"
+)
+
+func main() {
+	// Note: YOUR-ACCESSKEYID, YOUR-SECRETACCESSKEY and my-bucketname are
+	// dummy values, please replace them with original values.
+
+	// API requests are secure (HTTPS) if secure=true and insecure (HTTP) otherwise.
+	// New returns an MinIO Admin client object.
+	madmClnt, err := madmin.New("your-minio.example.com:9000", "YOUR-ACCESSKEYID", "YOUR-SECRETACCESSKEY", true)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// Print most recent failures for replication across all nodes in a minio cluster.
+	mrfCh := madmClnt.BucketReplicationMRF(context.Background(), "my-bucketname", "all")
+	for m := range mrfCh {
+		if m.Err != nil {
+			log.Fatalln(m.Err)
+		}
+		fmt.Println(m)
+	}
+}
