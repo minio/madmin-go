4b2fff2a6552ba48d3e02520f9b6acceb0231734
yzewei <yangzewei@loongson.cn>
2024-01-18 16:47:12 +0800

    Add loongarch64 support
    Signed-off-by: yzewei <yangzewei@loongson.cn>

--- kernel/kernel_utsname_int8.go
+++ kernel/kernel_utsname_int8.go
@@ -17,8 +17,8 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 //
 
-//go:build (linux && 386) || (linux && amd64) || (linux && arm64) || (linux && loong64) || (linux && mips64) || (linux && mips64le) || (linux && mips)
-// +build linux,386 linux,amd64 linux,arm64 linux,loong64 linux,mips64 linux,mips64le linux,mips
+//go:build (linux && 386) || (linux && amd64) || (linux && arm64) || (linux && loong64) || (linux && mips64) || (linux && mips64le) || (linux && mips) || (linux && loong64)
+// +build linux,386 linux,amd64 linux,arm64 linux,loong64 linux,mips64 linux,mips64le linux,mips linux,loong64
 
 package kernel
 
