7fc3d3e2b540c377173ef59a4ca4f91ad95335ef
Harshavardhana <harsha@minio.io>
2025-02-28 00:16:53 -0800

    logger entry must point to madmin-go/v4

--- api-error-response.go
+++ api-error-response.go
@@ -112,7 +112,7 @@ func httpRespToErrorResponse(resp *http.Response) error {
 //
 // For example:
 //
-//	import admin "github.com/minio/madmin-go/v3"
+//	import admin "github.com/minio/madmin-go/v4"
 //	...
 //	...
 //	ss, err := adm.ServiceStatus(...)
