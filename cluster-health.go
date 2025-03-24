6c18398dc0843256e1bdf3abd7962fe4737c139d
Shubhendu Ram Tripathi <shubhendu@minio.io>
2025-03-24 13:06:26 +0530

    review comments
    - remove LBendpoint flag
    - Use target level flags
    
    Signed-off-by: Shubhendu Ram Tripathi <shubhendu@minio.io>

--- cluster-health.go
+++ cluster-health.go
@@ -25,7 +25,6 @@ import (
 	"net/http/httptrace"
 	"net/url"
 	"strconv"
-	"strings"
 	"sync"
 	"time"
 )
@@ -135,7 +134,6 @@ type AliveResult struct {
 	DNSResolveTime time.Duration `json:"dnsResolveTime"`
 	Online         bool          `json:"online"` // captures x-minio-server-status
 	Error          error         `json:"error"`
-	IsLBEndpoint   bool          `json:"isLBEndpoint"`
 }
 
 // Alive will hit `/minio/health/live` to check if server is reachable, optionally returns
@@ -213,13 +211,6 @@ func (an *AnonymousClient) alive(ctx context.Context, u *url.URL, resource strin
 		relPath:          resource,
 		endpointOverride: u,
 	}, trace)
-	isLBEndpoint := false
-	for name, _ := range resp.Header {
-		if strings.HasPrefix(name, "X-Forwarded") || strings.HasPrefix(name, "X-Real-IP") {
-			isLBEndpoint = true
-			break
-		}
-	}
 	closeResponse(resp)
 	var respTime time.Duration
 	if firstByteTime.IsZero() {
@@ -232,7 +223,6 @@ func (an *AnonymousClient) alive(ctx context.Context, u *url.URL, resource strin
 		Endpoint:       u,
 		ResponseTime:   respTime,
 		DNSResolveTime: dnsDoneTime.Sub(dnsStartTime),
-		IsLBEndpoint:   isLBEndpoint,
 	}
 	if err != nil {
 		result.Error = err
