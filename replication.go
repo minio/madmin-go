8ec2f4ea533d191757c1736591a976579239af98
Shubhendu Ram Tripathi <shubhendu@minio.io>
2025-03-25 11:10:40 +0530

    struct renames
    Signed-off-by: Shubhendu Ram Tripathi <shubhendu@minio.io>

--- replication.go
+++ replication.go
@@ -25,58 +25,58 @@ import (
 	"github.com/minio/minio-go/v7/pkg/replication"
 )
 
-type ReplicationInfo struct {
-	Error             string             `json:"error,omitempty"`
-	SREnabled         bool               `json:"site_replication_enabled"`
-	ActiveWorkers     WorkerStat         `json:"active_workers,omitempty"`
-	Queued            InQueueMetric      `json:"queued,omitempty"`
-	ReplicaCount      int64              `json:"replica_count,omitempty"`
-	ReplicaSize       int64              `json:"replica_size,omitempty"`
-	Proxying          bool               `json:"proxying,omitempty"`
-	Proxied           ReplProxyMetric    `json:"proxied,omitempty"`
-	Sites             []ReplicationSite  `json:"sites,omitempty"`
-	ReplicatedBuckets []ReplicatedBucket `json:"replicated_buckets,omitempty"`
+type ReplDiagInfo struct {
+	Error               string               `json:"error,omitempty"`
+	SREnabled           bool                 `json:"site_replication_enabled"`
+	ActiveWorkers       WorkerStat           `json:"active_workers,omitempty"`
+	Queued              InQueueMetric        `json:"queued,omitempty"`
+	ReplicaCount        int64                `json:"replica_count,omitempty"`
+	ReplicaSize         int64                `json:"replica_size,omitempty"`
+	Proxying            bool                 `json:"proxying,omitempty"`
+	Proxied             ReplProxyMetric      `json:"proxied,omitempty"`
+	Sites               []ReplDiagSite       `json:"sites,omitempty"`
+	RDReplicatedBuckets []ReplDiagReplBucket `json:"replicated_buckets,omitempty"`
 }
 
-type ReplicationSite struct {
-	Addr         string                  `json:"addr,omitempty"`
-	DeploymentID string                  `json:"deployment_id"`
-	Info         SiteReplicationSiteInfo `json:"info,omitempty"`
+type ReplDiagSite struct {
+	Addr         string           `json:"addr,omitempty"`
+	DeploymentID string           `json:"deployment_id"`
+	Info         ReplDiagSiteInfo `json:"info,omitempty"`
 }
 
-type SiteReplicationSiteInfo struct {
-	Nodes                []MinIONode         `json:"nodes,omitempty"`
-	LDAPEnabled          bool                `json:"ldap_enabled,omitempty"`
-	OpenIDEnabled        bool                `json:"openid_enabled,omitempty"`
-	BucketsCount         int                 `json:"buckets_count,omitempty"`
-	Edge                 bool                `json:"edge,omitempty"`
-	ILMEnabled           bool                `json:"ilm_enabled,omitempty"`
-	EncryptionEnabled    bool                `json:"encryption_enabled,omitempty"`
-	ILMExpiryReplication bool                `json:"ilm_expiry_replication,omitempty"`
-	ObjectLockingEnabled bool                `json:"object_locking_enabled,omitmepyt"`
-	Throttle             ReplicationThrottle `json:"throttle,omitempty"`
-	ReplicatedCount      int64               `json:"replicated_count,omitempty"`
-	ReplicatedSize       int64               `json:"replicated_size,omitempty"`
-	ResyncStatus         string              `json:"resync_status"`
+type ReplDiagSiteInfo struct {
+	Nodes                []ReplDiagNode   `json:"nodes,omitempty"`
+	LDAPEnabled          bool             `json:"ldap_enabled,omitempty"`
+	OpenIDEnabled        bool             `json:"openid_enabled,omitempty"`
+	BucketsCount         int              `json:"buckets_count,omitempty"`
+	Edge                 bool             `json:"edge,omitempty"`
+	ILMEnabled           bool             `json:"ilm_enabled,omitempty"`
+	EncryptionEnabled    bool             `json:"encryption_enabled,omitempty"`
+	ILMExpiryReplication bool             `json:"ilm_expiry_replication,omitempty"`
+	ObjectLockingEnabled bool             `json:"object_locking_enabled,omitmepyt"`
+	Throttle             ReplDiagThrottle `json:"throttle,omitempty"`
+	ReplicatedCount      int64            `json:"replicated_count,omitempty"`
+	ReplicatedSize       int64            `json:"replicated_size,omitempty"`
+	ResyncStatus         string           `json:"resync_status"`
 }
 
-type MinIONode struct {
-	Addr                string `json:"addr,omitempty"`
-	MinIOVersion        string `json:"minio_version,omitempty"`
-	Uptime              int64  `json:"uptime,omitempty"`
-	PoolID              int    `json:"poolid,omitempty"`
-	SetID               int    `json:"setid,omitempty"`
-	IsLeader            bool   `json:"is_leader,omitempty"`
-	ILMExpiryInProgress bool   `json:"ilm_expiry_in_progress,omitempty"`
+type ReplDiagNode struct {
+	Addr         string `json:"addr,omitempty"`
+	MinIOVersion string `json:"minio_version,omitempty"`
+	Uptime       int64  `json:"uptime,omitempty"`
+	PoolID       int    `json:"poolid,omitempty"`
+	// SetID               int    `json:"setid,omitempty"`
+	IsLeader            bool `json:"is_leader,omitempty"`
+	ILMExpiryInProgress bool `json:"ilm_expiry_in_progress,omitempty"`
 }
 
-type ReplicatedBucket struct {
-	Name               string                `json:"name,omitempty"`
-	ReplicationInfo    BucketReplicationInfo `json:"replication_info,omitempty"`
-	ReplicationTargets []ReplicationTarget   `json:"replication_targets,omitempty"`
+type ReplDiagReplBucket struct {
+	Name               string                     `json:"name,omitempty"`
+	ReplicationInfo    ReplDiagBucketReplInfo     `json:"replication_info,omitempty"`
+	ReplicationTargets []ReplDiagBucketReplTarget `json:"replication_targets,omitempty"`
 }
 
-type ReplicationTarget struct {
+type ReplDiagBucketReplTarget struct {
 	SourceBucket              string        `json:"source_bucket,omitempty"`
 	TargetBucket              string        `json:"target_bucket,omitempty"`
 	Addr                      string        `json:"addr,omitempty"`
@@ -98,38 +98,38 @@ type ReplicationTarget struct {
 	MetadataSync              bool          `json:"metadata_sync,omitempty"`
 }
 
-type BucketReplicationInfo struct {
-	VersionEnabled   bool               `json:"version_enabled,omitempty"`
-	ObjectLocking    bool               `json:"object_locking,omitempty"`
-	ExcludedPrefixes []string           `json:"excluded_prefixes,omitempty"`
-	ILM              ReplicationILMInfo `json:"ilm,omitempty"`
-	Encryption       ReplicationEncInfo `json:"encryption,omitempty"`
-	Config           replication.Config `json:"config,omitempty"`
-	Resync           BucketResyncInfo   `json:"resync,omitempty"`
+type ReplDiagBucketReplInfo struct {
+	VersionEnabled   bool                     `json:"version_enabled,omitempty"`
+	ObjectLocking    bool                     `json:"object_locking,omitempty"`
+	ExcludedPrefixes []string                 `json:"excluded_prefixes,omitempty"`
+	ILM              ReplDiagILMInfo          `json:"ilm,omitempty"`
+	Encryption       ReplDiagEncInfo          `json:"encryption,omitempty"`
+	Config           replication.Config       `json:"config,omitempty"`
+	Resync           ReplDiagBucketResyncInfo `json:"resync,omitempty"`
 }
 
-type ReplicationILMInfo struct {
-	Enabled bool                 `json:"enabled,omitempty"`
-	Rules   []ReplicationILMRule `json:"rules,omitempty"`
+type ReplDiagILMInfo struct {
+	Enabled bool              `json:"enabled,omitempty"`
+	Rules   []ReplDiagILMRule `json:"rules,omitempty"`
 }
 
-type ReplicationILMRule struct {
+type ReplDiagILMRule struct {
 	ID         string `json:"id,omitempty"`
 	Expiration bool   `json:"expiration,omitempty"`
 	Transition bool   `json:"transition,omitempty"`
 }
 
-type ReplicationEncInfo struct {
+type ReplDiagEncInfo struct {
 	Enabled  bool            `json:"enabled,omitempty"`
-	EncRules []BucketEncRule `json:"enc_rules,omitempty"`
+	EncRules []BucketEncInfo `json:"enc_rules,omitempty"`
 }
 
-type BucketEncRule struct {
+type BucketEncInfo struct {
 	Algorithm string `json:"algorithm,omitempty"`
 	EncKey    string `json:"enc_key,omitempty"`
 }
 
-type BucketResyncInfo struct {
+type ReplDiagBucketResyncInfo struct {
 	InProgress      bool      `json:"in_progress,omitempty"`
 	StartTime       time.Time `json:"start_time,omitempty"`
 	FailedCount     int64     `json:"failed_count,omitempty"`
@@ -138,7 +138,7 @@ type BucketResyncInfo struct {
 	ReplicatedSize  int64     `json:"replicated_size,omitempty"`
 }
 
-type ReplicationThrottle struct {
+type ReplDiagThrottle struct {
 	IsSet bool   `json:"is_set,omitempty"`
 	Limit uint64 `json:"limit,omitempty"`
 }
