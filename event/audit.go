72b7c7ff24432a51a087aee6a052fd8ebc968941
Praveenrajmani <praveen@min.io>
2025-04-09 15:45:57 +0530

    Introduce API, Error event types and subsystem for event recorder

---
+++ event/audit.go
@@ -0,0 +1,37 @@
+// Copyright (c) 2015-2025 MinIO, Inc.
+//
+// This file is part of MinIO Object Storage stack
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package event
+
+import "time"
+
+//go:generate msgp $GOFILE
+
+// Audit represents the user triggered audit events
+type Audit struct {
+	Version    string                 `json:"version"`
+	Time       time.Time              `json:"time"`
+	Node       string                 `json:"node,omitempty"`
+	APIName    string                 `json:"apiName,omitempty"`
+	Bucket     string                 `json:"bucket,omitempty"`
+	Tags       map[string]string      `json:"tags,omitempty"`
+	RequestID  string                 `json:"requestID,omitempty"`
+	ReqClaims  map[string]interface{} `json:"requestClaims,omitempty"`
+	SourceHost string                 `json:"sourceHost,omitempty"`
+	AccessKey  string                 `json:"accessKey,omitempty"`
+	ParentUser string                 `json:"parentUser,omitempty"`
+}
