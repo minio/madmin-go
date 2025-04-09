632e2c4f3e9a40ae6b0e23c85507b1a4747e7096
Praveenrajmani <praveen@min.io>
2025-04-09 15:45:57 +0530

    feat: introduce event recorder types and implementation

---
+++ api-events.go
@@ -0,0 +1,75 @@
+//
+// Copyright (c) 2015-2025 MinIO, Inc.
+//
+// This file is part of MinIO Object Storage stack
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+
+package madmin
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"net/url"
+
+	"github.com/minio/madmin-go/v4/event"
+)
+
+// GetAPIEvents fetches the persisted API events from MinIO
+func (adm AdminClient) GetAPIEvents(ctx context.Context, node string, api string) <-chan event.API {
+	eventCh := make(chan event.API)
+
+	// Only success, start a routine to start reading line by line.
+	go func(eventCh chan<- event.API) {
+		defer close(eventCh)
+		urlValues := make(url.Values)
+		urlValues.Set("node", node)
+		urlValues.Set("api", api)
+		reqData := requestData{
+			relPath:     adminAPIPrefix + "/events/api",
+			queryValues: urlValues,
+		}
+		resp, err := adm.executeMethod(ctx, http.MethodGet, reqData)
+		if err != nil {
+			closeResponse(resp)
+			return
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			return
+		}
+		dec := json.NewDecoder(resp.Body)
+		for {
+			var info event.API
+			if err = dec.Decode(&info); err != nil {
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				continue
+			}
+			select {
+			case <-ctx.Done():
+				return
+			case eventCh <- info:
+			}
+		}
+
+	}(eventCh)
+
+	return eventCh
+}
