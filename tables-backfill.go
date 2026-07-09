f00197cc2d7c1654d0a9d4b553b16822022b5d7f
Raul <gfxsraul@gmail.com>
2026-07-09 19:12:51 +0300

    Fix
    Signed-off-by: Raul <gfxsraul@gmail.com>

--- tables-backfill.go
+++ tables-backfill.go
@@ -42,7 +42,7 @@ type BackfillResult struct {
 	Namespaces int      `json:"namespaces"`
 	Tables     int      `json:"tables"`
 	Views      int      `json:"views"`
-	Functions  int      `json:"functions,omitempty"`
+	Functions  int      `json:"functions"`
 	Warnings   []string `json:"warnings,omitempty"`
 }
 
